d11a96fec4774aa00acde01b30ea5e31042c34cc
rmenai <rami.menai@outlook.com>
2022-12-16 20:17:33 +0300

    Create NavBar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex h-12 w-full items-center bg-blue-200 px-2">
+    <NuxtLink v-if="$route.path !== localePath('/')" :to="localePath('/')">
+      <Icon class="text-4xl" name="ic:baseline-arrow-back" />
+    </NuxtLink>
+  </div>
+</template>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="font-inter relative min-h-screen bg-transparent">
-    <div class="absolute z-50 flex h-12 w-full items-center bg-blue-200 px-2">
-      <NuxtLink v-if="$route.path !== localePath('/')" :to="localePath('/')">
-        <Icon class="text-4xl" name="ic:baseline-arrow-back" />
-      </NuxtLink>
-    </div>
-    <div class="absolute h-full w-full">
-      <slot />
+    <div class="absolute flex h-full w-full flex-col">
+      <NavBar />
+      <div class="h-full w-full grow">
+        <slot />
+      </div>
     </div>
   </div>
 </template>
