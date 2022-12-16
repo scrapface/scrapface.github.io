8a70c1a7e246e648d9a44f617f8c7e2cb7210561
rmenai <rami.menai@outlook.com>
2022-12-16 12:00:26 +0300

    Add basic layout and settings page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="flex h-full flex-col items-center justify-center">
-    <div class="flex flex-col items-center gap-2 text-4xl">
-      <p class="font-semibold">Connect cable</p>
-      <Icon name="ic:baseline-cable" />
+    <div class="flex h-[300px] w-[600px] flex-wrap items-center justify-center gap-12">
+      <NuxtLink :to="localePath('/')"><PageBox name="Feature" /></NuxtLink>
+      <NuxtLink :to="localePath('/')"><PageBox name="Navigation" /></NuxtLink>
+      <NuxtLink :to="localePath('/settings')"><PageBox name="Settings" /></NuxtLink>
     </div>
   </div>
 </template>
