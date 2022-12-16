b33c3080412f178e28b00c8bd1dd067a9b64190b
rmenai <rami.menai@outlook.com>
2022-12-16 20:32:16 +0300

    Add basic devices page layout

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex h-full flex-col items-center justify-center">
     <div class="flex h-[300px] w-[600px] flex-wrap items-center justify-center gap-12">
-      <NuxtLink :to="localePath('/')"><PageBox name="Feature" /></NuxtLink>
-      <NuxtLink :to="localePath('/')"><PageBox name="Navigation" /></NuxtLink>
+      <NuxtLink :to="localePath('/devices')"><PageBox name="Devices" /></NuxtLink>
+      <NuxtLink :to="localePath('/')"><PageBox name="Scenes" /></NuxtLink>
       <NuxtLink :to="localePath('/settings')"><PageBox name="Settings" /></NuxtLink>
     </div>
   </div>
